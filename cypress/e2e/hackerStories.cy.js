9056c9b2bd1ce3d78845e31d488081482fd820d9
Raquel <raquel.carneiro@cilia.com.br>
2024-11-28 16:09:28 -0300

    primeiros cenarios de teste

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -1,33 +1,115 @@
 describe('Hacker Stories', () => {
-  it('mostrar 100 histórias e depois mais 100 depois de clicar em "More"', () => {})
+  const initialTerm = 'redux'
+  const newTerm = 'cypress'
+  const stories = require('../fixtures/stories.json')
 
-  it('mostra a história certa para cada história renderizada', () => {})
+  context('API real', () => {
+    beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: 'redux',
+          page: '0'
+        }
+      }).as('getStoriesReal')
 
-  it('mostra uma história a menos após clicar em "Dismiss" para uma história', () => {})
+      cy.visit('/')
+      cy.wait('@getStoriesReal')
+    })
+
+    it('mostrar 100 histórias e depois mais 100 depois de clicar em "More"', () => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: initialTerm,
+          page: '1'
+        }
+      }).as('getMoreStories')
+      cy.get('.table > *').should('have.length', 101)
+
+      cy.contains('More')
+        .should('be.visible')
+        .click()
+
+      cy.wait('@getMoreStories')
+
+      cy.get('.table > *').should('have.length', 201)
+    })
+  })
+
+  context('API mock', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'GET',
+        `**/search?query=${initialTerm}*`,
+        { fixture: 'stories' }
+      ).as('getStoriesMock')
+
+      cy.visit('/')
+      cy.wait('@getStoriesMock')
+    })
+    it('mostra a história certa para cada história renderizada', () => {
+
+      cy.get('.table > *').eq(1)
+        .should('contain', stories.hits[0].title)
+        .and('contain', stories.hits[0].author)
+        .and('contain', stories.hits[0].num_comments)
+        .and('contain', stories.hits[0].points)
+
+      cy.get('.table > *').eq(2)
+        .should('contain', stories.hits[1].title)
+        .and('contain', stories.hits[1].author)
+        .and('contain', stories.hits[1].num_comments)
+        .and('contain', stories.hits[1].points)
+
+      cy.get('.table > *').eq(3)
+        .should('contain', stories.hits[2].title)
+        .and('contain', stories.hits[2].author)
+        .and('contain', stories.hits[2].num_comments)
+        .and('contain', stories.hits[2].points)
+
+      cy.get('.table > *').eq(4)
+        .should('contain', stories.hits[3].title)
+        .and('contain', stories.hits[3].author)
+        .and('contain', stories.hits[3].num_comments)
+        .and('contain', stories.hits[3].points)
+    })
+
+    it('mostra uma história a menos após clicar em "Dismiss" para uma história', () => {
+      cy.get(':nth-child(2) > :nth-child(5) > .button-inline')
+        .should('be.visible')
+        .click()
+
+      cy.get('.table > *').should('have.length', 4)
+    })
+  })
 
   context('Ordenação', () => {
-    it('ordenar por título', () => {})
+    it.skip('ordenar por título', () => { })
 
-    it('ordenar por autor', () => {})
-  
-    it('ordenar por número de comentários', () => {})
-  
-    it('ordenar por número de pontos', () => {})
+    it.skip('ordenar por autor', () => { })
+
+    it.skip('ordenar por número de comentários', () => { })
+
+    it.skip('ordenar por número de pontos', () => { })
   })
 
   context('Pesquisa', () => {
-    it('mostrar nenhuma história quando nenhuma história for encontrada', () => {})
+    it.skip('mostrar nenhuma história quando nenhuma história for encontrada', () => { })
 
-    it('pesquisar depois de digitar e clicar em ENTER', () => {})
-  
-    it('pesquisar depois de digitar e clicar no botão "Search"', () => {})
-  
-    it('pesquisar e submeter a pesquisa diretamente', () => {})
+    it.skip('pesquisar depois de digitar e clicar em ENTER', () => { })
+
+    it.skip('pesquisar depois de digitar e clicar no botão "Search"', () => { })
+
+    it.skip('pesquisar e submeter a pesquisa diretamente', () => { })
   })
 
-  context('Erros', () => {
-    it('server error', () => {})
 
-    it('network error', () => {})
+  context('Erros', () => {
+    it.skip('server error', () => { })
+
+    it.skip('network error', () => { })
   })
 })
